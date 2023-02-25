118dd79e055b65a75704bbe7eefc3f429e9e04e2
luke0han <lukeohanlon@outlook.com>
2023-02-25 11:52:07 +0000

    home- quote, services, imgs, testimonial

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
-sidebar = () => {
+const slideCont = document.getElementById("slides-container");
+const slide = document.querySelector(".slide");
+const prevButton = document.getElementById("slide-arrow-prev");
+const nextButton = document.getElementById("slide-arrow-next");
 
-}
+nextButton.addEventListener("click", () => {
+  const slideWidth = slide.clientWidth;
+  slideCont.scrollLeft += slideWidth;
+});
+
+prevButton.addEventListener("click", () => {
+  const slideWidth = slide.clientWidth;
+  slideCont.scrollLeft -= slideWidth;
+});
