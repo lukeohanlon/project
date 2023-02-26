f65742c6969538c9dbf95360f5a1163a8e9b6678
luke0han <lukeohanlon@outlook.com>
2023-02-26 13:20:59 +0000

    Added photos

--- index.js
+++ index.js
@@ -12,3 +12,31 @@ prevButton.addEventListener("click", () => {
   const slideWidth = slide.clientWidth;
   slideCont.scrollLeft -= slideWidth;
 });
+
+const inputs = document.querySelectorAll(".input");
+
+function focusFunc() {
+  let parent = this.parentNode;
+  parent.classList.add("focus");
+}
+
+function blurFunc() {
+  let parent = this.parentNode;
+  if (this.value == "") {
+    parent.classList.remove("focus");
+  }
+}
+
+inputs.forEach((input) => {
+  input.addEventListener("focus", focusFunc);
+  input.addEventListener("blur", blurFunc);
+});
+
+var form = document.getElementById('f');
+
+function sendSuccess() {
+  e.preventDefault()
+  if (form.checkValidity()) {
+    alert("Adding Succesful!");
+  }
+}
