63479a97f6fd26ff6d46c70d91e92879cca12034
Anna Samus <annapn32@gmail.com>
2023-03-24 18:28:19 +0000

    js

--- index.js
+++ index.js
@@ -64,4 +64,17 @@ function circleChange() {
 
 /* ******** Anna********* */
 
+let mybutton = document.getElementById("btn");
+window.onscroll = function() {scrollFunction()};
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
 
+function btnTop() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0; 
+}
